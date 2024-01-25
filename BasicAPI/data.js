556495abf413ad9a1a3c564e92c6fc9a7fb9ffe7
SurajBhalerao27 <cs.engr.suraj@gmail.com>
2024-01-25 17:06:57 +0530

    First commit

---
+++ BasicAPI/data.js
@@ -0,0 +1,6 @@
+const data = [
+    { name: 'Suraj', email: 'Suraj123@gmail.com' },
+    { name: 'Poms', email: 'poms3@gmail.com' },
+    { name: 'David', email: 'david808080@gmail.com' }
+]
+module.exports = data;
